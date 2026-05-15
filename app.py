c968631ef15aabfb13c7b45546623e5f9f352bb3
niklas983 <126800172+niklas983@users.noreply.github.com>
2026-05-15 09:25:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -138,10 +138,10 @@ st.title("🧪 Vlerësim i strategjive të depistimit të kancerit me anë të s
 
 st.markdown("""Mënyra e përdorimit:\n
         1. Vendos numrin e pacientëve dhe numrin e simulimeve\n
-        2. Vendos një emër për skenarit.\n
-        3. Përcakto parametrat e tij (grupmoshën, prevalencën e sëmundjes, sensitivitetin dhe specificitetin e testit)\n
-        4. Shtyp butonin "Shto skenar".\n
-        5. Përcakto kostot e testit. \n
+        2. Vendos një emër për skenarin.\n
+        3. Përcakto parametrat e tij (grupmoshën, prevalencën e sëmundjes, \n sensitivitetin dhe specificitetin e testit)\n
+        4. Përcakto kostot e testit.\n
+        5. Shtyp butonin "Shto skenar" \n
         6. Përsërit hapat nga 1 në 5 për të shtuar skenarë të tjerë.\n
         7. Shtyp butonin "Fillo simulimin" në të djathtë dhe shiko rezultatet.
 """)
